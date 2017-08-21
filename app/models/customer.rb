af314cb14e1bc3737050530cac8b13ba6ddbfc39
Bobby Huang <zhenghuahuang@gmail.com>
2017-08-21 10:25:41 -0400

    done

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,44 @@
 class Customer
-  attr_accessor :first_name, :last_name
+  attr_reader :first_name, :last_name
 
-  def initialize(first_name, last_name)
-    @first_name = first_name
-    @last_name  = last_name
+  ALL = []
+
+  def initialize(params = {})
+    params.each { |k,v| send("#{k}=", v)}
+    ALL << self
   end
 
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  def self.find_by_name(full_name)
+    ALL.detect do |customer|
+      customer.full_name == full_name
+    end
+  end
+
+  def self.find_all_by_first_name(first_name)
+    ALL.select do |customer|
+      customer.first_name == first_name
+    end
+  end
+
+  def self.all_names
+    ALL.map do |customer|
+      customer.full_name
+    end
+  end
+
+  def self.all
+    ALL
+  end
+
+# testing only
+  def self.clear_all
+    ALL.clear
+  end
+
+  private
+    attr_writer :first_name, :last_name
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,40 @@
 class Restaurant
-  attr_accessor :name
+  attr_reader :name
 
-  def initialize(name)
-    @name = name
+  ALL = []
+
+  def initialize(params = {})
+    params.each { |k, v| send("#{k}=", v)}
+    ALL << self
   end
 
+  def self.find_by_name(restaurant_name)
+    ALL.detect do |restaurant|
+      restaurant.name == restaurant_name
+    end
+  end
+
+  def reviews
+    Review.all.select do |review|
+      review.restaurant == self
+    end
+  end
+
+  def customers
+    self.reviews.map do |review|
+      review.customer
+    end
+  end
+
+  def self.all
+    ALL
+  end
+
+# testing only
+  def self.clear_all
+    ALL.clear
+  end
+
+  private
+    attr_writer :name
 end
