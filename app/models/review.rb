96e1e4e8ffd13e60ff61a65fab75ed8355947ce0
mgaspari <marcoanthonygaspari@gmail.com>
2017-08-21 10:36:03 -0400

    Done.

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,7 +2,7 @@ class Review
    @@all = []
    attr_accessor :customer, :restaurant, :content
 
-   def initialize(content)
+   def initialize(content, restaurant)
      customer.add_review(restaurant, content)
      @@all << self
    end
