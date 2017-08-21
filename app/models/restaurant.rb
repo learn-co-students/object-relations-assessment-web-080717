7bac0fd8321f91f35f7c357f5c80b9e71380ccd5
sophiehelf <sophie.helf@gmail.com>
2017-08-21 10:35:04 -0400

    Done.

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,34 @@
 class Restaurant
   attr_accessor :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def self.find_by_name(name)
+  	self.all.detect do |nom|
+  		nom.name == name 
+  	end
+  end
+
+  def reviews	
+  	Review.all.select do |review|
+  		review.restaurant == self 
+  	end
+  end
+
+  def customers
+  	customer_array = []
+  	restaurants = Review.all.select do |review| #gather array of reviews specific to that restaurant
+  		review.restaurant == self 
+  	end
+  	customer_reviews = restaurants.collect do |restaurant| # collect all the customer names of each review and store them in array
+  		restaurant.customer 
+  	end 
+  	customer_reviews
   end
 
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,26 @@
 class Review
-  
+ 	attr_accessor :customer, :restaurant, :content
+ 	
+	@@all = []
+
+	def initialize(customer, restaurant, content)
+		@customer = customer
+		@restaurant = restaurant 
+		@content = content
+		@@all << self 
+	end
+
+	def self.all
+		@@all 
+	end
+
+	def customer
+		@customer
+	end
+
+	def restaurant
+		@restaurant
+	end
+
 end
 
