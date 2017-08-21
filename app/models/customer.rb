febe50e8c10c62ef364b353af2ad1b10446192c1
LS-C <suzy.cho111@gmail.com>
2017-08-21 10:35:06 -0400

    Done.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,44 @@
+require 'pry'
+
+
 class Customer
-  attr_accessor :first_name, :last_name
+  attr_accessor :first_name, :last_name, :review
+
+  @@all = []
 
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
   end
 
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    first, last = name.split(" ")
+    self.all.find { |customer| customer.first_name == first && customer.last_name == last }
+  end
+
+  def self.all_names
+    self.all.map { |customer| customer.full_name }
+  end
+
+
+  def add_review(restaurant, content)
+        Restaurant.all.find { |restaurant| restaurant.name == restaurant }
+        review = Review.new(content)
+        review.customer = self
+
+  end
+
+
+
+
+
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,21 @@
 class Review
-  
+
+    attr_accessor :content, :restaurant, :customer
+
+    @@all = []
+
+    def initialize(restaurant, content)
+        @restaurant = restaurant
+        @content= content
+        @all << self
+    end
+
+
+    def
+
+
+
+
+
 end
 
