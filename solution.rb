ea7820eeacfc6837dc09b2514637cde9c4b2dd0a
github username <github email address>
2017-08-24 22:27:50 -0400

    added classes to solution

--- solution.rb
+++ solution.rb
@@ -18,24 +18,71 @@ class Restaurant
 
   def self.find_by_name(name)
     self.all.find do |restaurant|
-      restaurant == name
+      restaurant.name == name
     end
   end
 
   def reviews
     Review.all.select do |review|
-      review.restaurant == self.name
+      review.restaurant == self
     end
   end
 
   def customers
-    this_restos_reviews = Review.all.select do |review|
-      review.restaurant == self.name
-    end
-    this_restos_reviews.collect do |review|
+    self.reviews.collect do |review|
       review.customer
     end
   end
+
+end
+
+
+
+
+class Customer
+
+  @@all = []
+
+  attr_accessor :first_name, :last_name
+
+  def initialize(first_name, last_name)
+    @first_name = first_name
+    @last_name  = last_name
+    @@all << self
+  end
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+  def self.all
+    @@all
+  end
+
+
+  def self.find_by_name(full_name)
+    self.all.find do |name|
+      full_name == name
+    end
+  end
+
+
+  def self.find_all_by_first_name(first_name)
+    self.all.find do |customer|
+      customer.first_name
+    end
+  end
+
+  def self.all_names
+    Customer.all.collect do |customer|
+      customer.full_name
+    end
+  end
+
+  def add_review(restaurant, content, customer)
+    new_review = Review.new(restaurant, content, self)
+  end
+
 
 end
 
@@ -61,60 +108,6 @@ attr_accessor :restaurant, :customer
 
   def content
     @content
-  end
-
-
-end
-
-
-
-
-
-
-class Customer
-
-  @@all = []
-
-  attr_accessor :first_name, :last_name
-
-  def initialize(first_name, last_name)
-    @first_name = first_name
-    @last_name  = last_name
-  end
-
-  def full_name
-    "#{first_name} #{last_name}"
-  end
-
-  def self.all
-    Review.all.collect do |review|
-      review.customer
-    end
-  end
-
-
-  def self.find_by_name(full_name)
-    self.all.find do |name|
-      full_name == name
-    end
-  end
-
-
-  def self.find_all_by_first_name(first_name)
-    whole_name = self.all.select do |name|
-      first_name == name.split(" ")[0]
-    end
-    whole_name.split(" ")[0]
-  end
-
-  def self.all_names
-    Customer.all.collect do |name|
-      name
-    end
-  end
-
-  def add_review(restaurant, content, customer)
-    new_review = Review.new(restaurant, content, self)
   end
 
 
